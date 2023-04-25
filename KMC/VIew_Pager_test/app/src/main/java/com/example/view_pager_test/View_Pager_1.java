1c7331c967c089251f851d7269b4c2cbc6ac4c7a
alsckd99 <gpfznsalsckd@gmail.com>
2023-04-25 16:36:10 +0900

    add KMC

---
+++ KMC/VIew_Pager_test/app/src/main/java/com/example/view_pager_test/View_Pager_1.java
@@ -0,0 +1,53 @@
+package com.example.view_pager_test;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class View_Pager_1 extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+
+
+    private String mParam1;
+    private String mParam2;
+
+    public View_Pager_1() {
+        // Required empty public constructor
+    }
+
+    public static View_Pager_1 newInstance(String param1, String param2) {
+        View_Pager_1 fragment = new View_Pager_1();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_view__pager_1, container, false);
+    }
+}
