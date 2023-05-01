4f6affd36b8fa6dc51dd0470f83941a9201fbb61
alsckd99 <gpfznsalsckd@gmail.com>
2023-05-02 00:15:36 +0900

    add KMC/map_practice

--- KMC/map_practice/app/src/main/java/com/example/map_practice/MainActivity.java
+++ KMC/map_practice/app/src/main/java/com/example/map_practice/MainActivity.java
@@ -23,7 +23,9 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -40,6 +42,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("locations/"+FirebaseDatabase.getInstance().getCu)
 
         //Map Fragment
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
